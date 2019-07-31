d911844836622cabf481f729f04acc5c5e80e95e
evrintobing <evrintobing@gmail.com>
2019-08-01 00:06:33 +0700

    Update

--- index.php
+++ index.php
@@ -30,48 +30,52 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<title>Azure Image Analyzer</title>
-		<link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
-		<!-- Bootstrap core CSS -->
-		<link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
-		<!-- Custom styles for this template -->
-		<link href="starter-template.css" rel="stylesheet">
-	</head>
+ <head>
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- <link rel="icon" href="https://raw.githubusercontent.com/muhrizky/Smart-Parkir/master/parking_meter__2__Mrq_icon.ico"> -->
 
-	<body>
-			<main role="main" class="container">
-				<div class="starter-template"> <br><br><br>
-					<h1>Upload Image</h1>
-					<br>
-					<p class="lead">Pilih Gambar</p>
-					<span class="border-top my-3"></span>
-				</div>
-				<div class="mt-4 mb-2">
-					<form class="d-flex justify-content-lefr" action="index.php" method="post" enctype="multipart/form-data">
-						<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required>
-						<input type="submit" name="submit" value="Upload">
-					</form>
-				</div>
-				<br>
-        <div
-        <table class='table table-hover'>
-					<thead>
-						<tr>
-							<th>Nama File</th>
-							<th>Alamat URL File</th>
-							<th>Action</th>
-						</tr>
-					</thead>
+    <title>Image Analyzer</title>
 
-					<tbody>
-						<?php
-						do {
-							foreach ($result->getBlobs() as $blob) {
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+  </head>
+<body>
+	<main role="main" class="container">
+    		<div class="starter-template"> <br>
+        		<h1>Image Analyzer</h1>
+				<p class="lead">Pilih foto dari komputer</p>
+				<span class="border-top my-3"></span>
+			</div>
+		<div class="mt-4 mb-2">
+			<form class="d-flex justify-content-lefr" action="index.php" method="post" enctype="multipart/form-data">
+				<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="">
+				<input type="submit" name="submit" value="Upload">
+			</form>
+		</div>
+		<br>
+		<br>
+		<h4>Total Files : <?php echo sizeof($result->getBlobs())?></h4>
+		<table class='table table-hover'>
+			<thead>
+				<tr>
+					<th>File Name</th>
+					<th>File URL</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				do {
+					foreach ($result->getBlobs() as $blob)
+					{
 						?>
 						<tr>
 							<td><?php echo $blob->getName() ?></td>
@@ -79,18 +83,24 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
 							<td>
 								<form action="analisis.php" method="post">
 									<input type="hidden" name="url" value="<?php echo $blob->getUrl()?>">
-									<input type="submit" name="submit" value="Detail Gambar" class="btn btn-primary">
+									<input type="submit" name="submit" value="Analyze!" class="btn btn-primary">
 								</form>
 							</td>
 						</tr>
 						<?php
-							} $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-						} while($result->getContinuationToken());
-						?>
-					</tbody>
-				</table>
-				</div>
+					}
+					$listBlobsOptions->setContinuationToken($result->getContinuationToken());
+				} while($result->getContinuationToken());
+				?>
+			</tbody>
+		</table>
 
+	</div>
 
-			</body>
-		</html>
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
